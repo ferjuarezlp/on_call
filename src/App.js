9ed4652f7513ab296cb3ef388e5dc688c3a4e3d6
Fer Juarez <fajuarez@teco.com.ar>
2021-07-05 12:25:37 -0300

    Added react-uuid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+
 import { useEffect, useState } from 'react';
 import './App.css';
+import uuid from 'react-uuid'
 
 function App() {
   const [date, setDate] = useState(null);
   //const [uniqueId, setUniqueId] = useState('');
-  const [uniqueId, setUniqueId] = useState(
+  const [uniqueId] = useState(
     localStorage.getItem('uniqueId') || ''
   );
 
   useEffect(() => {
-    localStorage.setItem('uniqueId', generateUUID());
+    localStorage.setItem('uniqueId', uuid());
  
     async function getDate() {
       const res = await fetch('/api/date');
@@ -21,20 +22,7 @@ function App() {
   }, []);
 
   
-  function generateUUID() { // Public Domain/MIT
-    var d = new Date().getTime();//Timestamp
-    var d2 = (performance && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16;//random number between 0 and 16
-        if(d > 0){//Use timestamp until depleted
-            r = (d + r)%16 | 0;
-            d = Math.floor(d/16);
-        } else {//Use microseconds since page-load if supported
-            r = (d2 + r)%16 | 0;
-            d2 = Math.floor(d2/16);
-        }
-        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-    });
+  
 }
 
   
