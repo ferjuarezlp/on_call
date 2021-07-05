3e68d22e82e702deaa2b0606327aef9214bd35f4
Fer Juarez <fajuarez@teco.com.ar>
2021-07-05 12:02:20 -0300

    Added unique id

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 
 function App() {
   const [date, setDate] = useState(null);
+  //const [uniqueId, setUniqueId] = useState('');
+  const [uniqueId, setUniqueId] = useState(
+    localStorage.getItem('uniqueId') || ''
+  );
+
   useEffect(() => {
+    localStorage.setItem('uniqueId', generateUUID());
+ 
     async function getDate() {
       const res = await fetch('/api/date');
       const newDate = await res.text();
@@ -12,9 +19,31 @@ function App() {
     }
     getDate();
   }, []);
+
+  
+  function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = (performance && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+  
   return (
     <main>
       <h1>Create React App + Go API</h1>
+      <h1>Unique ID is</h1>
+      <h1>{uniqueId}</h1>
+      
       <h2>
         Deployed with{' '}
         <a
