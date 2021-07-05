5e6b2e5e04b8bcdf2d7a3ff618f7cf32a52093af
Fer Juarez <fajuarez@teco.com.ar>
2021-07-05 12:21:05 -0300

    Removed text

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
     <main>
       <h1>Create React App + Go API</h1>
       <h1>Unique ID is</h1>
-      <h1>{uniqueId}</h1>
+      <h1>{uniqueId}s</h1>
       
       <h2>
         Deployed with{' '}
@@ -55,27 +55,6 @@ function App() {
         </a>
         !
       </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
       <br />
       <h2>The date according to Go is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
