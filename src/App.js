4023f7f48cbdf3f7a049dbab1bf31d6a79d3133b
Fer Juarez <fajuarez@teco.com.ar>
2021-07-26 11:45:11 -0300

    Added fingerprint lib

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
-import { useEffect, useState } from 'react';
+import React, { Component, useState, useEffect }   from 'react'
 import './App.css';
 import uuid from 'react-uuid'
 
 function App() {
-  const [date, setDate] = useState(null);
+  //const [date, setDate] = useState(null);
 
   const [uniqueId] = useState(
     localStorage.getItem('uniqueId') || ''
@@ -13,24 +13,19 @@ function App() {
   useEffect(() => {
     localStorage.setItem('uniqueId', uuid());
  
-    async function getDate() {
+    /*async function getDate() {
       const res = await fetch('/api/date');
       const newDate = await res.text();
-      setDate(newDate);
+      //setDate(newDate);
     }
-    getDate();
+    getDate();*/
   }, []);
 
-  
-  
-}
-
-  
   return (
     <main>
       <h1>Create React App + Go API</h1>
-      <h1>Unique ID is</h1>
-      <h1>{uniqueId}s</h1>
+      <h1>Unique ID is {uniqueId}</h1>
+      
       
       <h2>
         Deployed with{' '}
@@ -45,9 +40,14 @@ function App() {
       </h2>
       <br />
       <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      
     </main>
   );
+  
+}
+
+  
+  
 
 
 export default App;
